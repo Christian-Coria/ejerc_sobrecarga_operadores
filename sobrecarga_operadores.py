5f837a8432da941775267893c56837e8a1cbfb2d
Christian-Coria <104704311+Christian-Coria@users.noreply.github.com>
2022-05-09 17:26:56 -0300

    Update sobrecarga_operadores.py

---
+++ sobrecarga_operadores.py
@@ -0,0 +1,24 @@
+#un mismo operador puede comportarse de manera distinta 
+#ej operador + se maneja dist en stream que en un int o un mas entre listas 
+print("lista de sobreescritura")
+#para clases se necesita sobreescribir el operador (la sobreescritura y tiene que ver con la herencia)
+#      +  __add__(self,other)
+#      -  __sub__(self,other)
+#      *  __mul__(self,other)
+#      /  __truediv__(self,other)
+#      //  __floordiv__(self,other)
+#      %   __mod__(self,other)
+#      **  __pow__(self,other)
+
+#      -=  __isub__(self,other)
+#      +=  __iadd__(self,other)
+#      *=  __imul__(self,other)
+#      /=  __idiv__(self,other)
+#      //= __ifloordiv__(self,other)
+#      %=  __imod__(self,other)
+#      **=  __ipow__(self,other)
+
+#      -  __neg__(self,other)
+#      +  __pos__(self,other)
+
+
